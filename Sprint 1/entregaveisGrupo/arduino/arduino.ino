5f94dfb8371edb518faa4a0c7911d43227f70a1f
Vinicius Ventura Cavalcante <vinicius.cavalcante@sptech.school>
2025-06-05 13:15:04 -0300

    codigo arduino atualizado

--- Sprint 1/entregaveisGrupo/arduino/arduino.ino
+++ Sprint 1/entregaveisGrupo/arduino/arduino.ino
@@ -11,11 +11,29 @@ void loop() {
   temperatura = ((valorLeitura * 5.0 / 1023.0) / 0.01)-50;
   int temperatura2 = random(-24, -10);
   int temperatura3 = random(-24, -10);
-
-  
+  int temperatura4 = random(-24, -10);
+  int temperatura5 = random(-24, -10);
+  int temperatura6 = random(-24, -10);
+  int temperatura7 = random(-24, -10);
+  int temperatura8 = random(-24, -10);
+  int temperatura9 = random(-24, -10);
+  int temperatura10 = random(-24, -10);
+  int temperatura11 = random(-24, -10);
+  int temperatura12 = random(-24, -10);
+  int temperatura13 = random(-24, -10);
+  int temperatura14 = random(-24, -10);
+  int temperatura15 = random(-24, -10);
+  int temperatura16 = random(-24, -10);
+  int temperatura17 = random(-24, -10);
+  int temperatura18 = random(-24, -10);
+  int temperatura19 = random(-24, -10);
+  int temperatura20 = random(-24, -10);
+  int temperatura21 = random(-24, -10);
+  int temperatura22 = random(-24, -10);
+  int temperatura23 = random(-24, -10);
+  int temperatura24 = random(-24, -10);
 
 // caminhão 1
-  
   Serial.print(temperatura);
   Serial.print(";");
   Serial.print(temperatura2);
@@ -24,20 +42,55 @@ void loop() {
   Serial.print(";");
   
 // caminhão 2  
-  Serial.print(temperatura+10);
+  Serial.print(temperatura4);
   Serial.print(";");
-  Serial.print(temperatura2+10);
+  Serial.print(temperatura5);
   Serial.print(";");
-  Serial.print(temperatura3+10);
+  Serial.print(temperatura6);
   Serial.print(";");
   
 // caminhão 3
-  Serial.print(temperatura+10);
+  Serial.print(temperatura7);
   Serial.print(";");
-  Serial.print(temperatura2);
+  Serial.print(temperatura8);
   Serial.print(";");
-  Serial.println(temperatura3);
+  Serial.println(temperatura9);
+    
+// caminhão 4
+  Serial.print(temperatura10);
+  Serial.print(";");
+  Serial.print(temperatura11);
+  Serial.print(";");
+  Serial.println(temperatura12);
+    
+// caminhão 5
+  Serial.print(temperatura13);
+  Serial.print(";");
+  Serial.print(temperatura14);
+  Serial.print(";");
+  Serial.println(temperatura15);
+    
+// caminhão 6
+  Serial.print(temperatura16);
+  Serial.print(";");
+  Serial.print(temperatura17);
+  Serial.print(";");
+  Serial.println(temperatura18);
+    
+// caminhão 7
+  Serial.print(temperatura19);
+  Serial.print(";");
+  Serial.print(temperatura20);
+  Serial.print(";");
+  Serial.println(temperatura21);
+    
+// caminhão 8
+  Serial.print(temperatura22);
+  Serial.print(";");
+  Serial.print(temperatura23);
+  Serial.print(";");
+  Serial.println(temperatura24);
 
-  int one_minute = 1000;
+  int one_minute = 1000 * 20;
   delay(one_minute);
 }
