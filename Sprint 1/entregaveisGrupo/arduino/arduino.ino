120651c5103b311f29089aefee4b8569eaa20bdd
Henry <henry.arcaya@sptech.school>
2025-06-05 13:00:18 -0300

    simulação de 3 caminhões

--- Sprint 1/entregaveisGrupo/arduino/arduino.ino
+++ Sprint 1/entregaveisGrupo/arduino/arduino.ino
@@ -8,15 +8,36 @@ void setup() {
 
 void loop() {
   int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA);
-  temperatura = ((valorLeitura * 5.0 / 1023.0) / 0.01)-40;
-  int temperatura2 = random(-24, -12);
-  int temperatura3 = random(-24, -12);
+  temperatura = ((valorLeitura * 5.0 / 1023.0) / 0.01)-50;
+  int temperatura2 = random(-24, -10);
+  int temperatura3 = random(-24, -10);
+
+  
+
+// caminhão 1
   
   Serial.print(temperatura);
   Serial.print(";");
   Serial.print(temperatura2);
   Serial.print(";");
+  Serial.print(temperatura3); 
+  Serial.print(";");
+  
+// caminhão 2  
+  Serial.print(temperatura+10);
+  Serial.print(";");
+  Serial.print(temperatura2+10);
+  Serial.print(";");
+  Serial.print(temperatura3+10);
+  Serial.print(";");
+  
+// caminhão 3
+  Serial.print(temperatura+10);
+  Serial.print(";");
+  Serial.print(temperatura2);
+  Serial.print(";");
   Serial.println(temperatura3);
 
-  delay(1000);
+  int one_minute = 1000;
+  delay(one_minute);
 }
